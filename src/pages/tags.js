02632793ef87be2891ca870b39c872c33af3d37b
向松涛 <280304286@163.com>
2018-03-11 19:15:18 +0800

    chore: Tags page style modify

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -2,17 +2,13 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 import kebabCase from 'lodash/kebabCase';
-import Pagination from '../components/Pagination';
 import Header from '../components/Header';
 
 export default class TagsPageRoute extends React.Component {
     render() {
         const allTags = this.props.data.allMarkdownRemark.group;
-        const { allMarkdownRemark, site } = this.props.data;
-        const { edges } = allMarkdownRemark;
-        const { title, cover, tagCover, logo, navigation, description } = site.siteMetadata;
-        const { totalCount } = this.props.data.allMarkdownRemark;
-        const { tag } = this.props.pathContext;
+        const { site } = this.props.data;
+        const { title, cover, tagCover, logo, navigation } = site.siteMetadata;
         const coverImage = tagCover ? tagCover : (cover ? cover : false);
         return (
             <div>
@@ -27,23 +23,17 @@ export default class TagsPageRoute extends React.Component {
                     <h1 className="blog-name">All Tags</h1>
                     <span className="blog-description">Posts: {allTags.length}</span>
                 </Header>
-                <div className="container ">
-                    <main className="content content-tags" role="main">
-                        {allTags.map(tag =>
-                            <article className="tags" key={tag.fieldValue}>
-                                <div className="inner">
-                                    <Link
-                                        style={{
-                                            textDecoration: 'none',
-                                        }}
-                                        to={`/tag/${kebabCase(tag.fieldValue)}/`}
-                                    >
-                                        {tag.fieldValue} ({tag.totalCount})
+                <div className="container">
+                    <main className="content" role="main">
+                        <div className="content-tags">
+                            {allTags.map(tag =>
+                                <span className="tags" key={tag.fieldValue}>
+                                    <Link to={`/tag/${kebabCase(tag.fieldValue)}/`}>
+                                        {tag.fieldValue}({tag.totalCount})
                                     </Link>
-                                </div>
-                            </article>
-                        )}
-                        <Pagination page="1" pages="1" />
+                                </span>
+                            )}
+                        </div>
                     </main>
                 </div>
             </div>
@@ -52,7 +42,7 @@ export default class TagsPageRoute extends React.Component {
 }
 
 export const pageQuery = graphql`
-  query TagsQuery($skip: Int = 0, $limit: Int = 3) {
+  query TagsQuery {
     site {
       siteMetadata {
         ...siteFrag
@@ -61,7 +51,7 @@ export const pageQuery = graphql`
     allMarkdownRemark(
       filter: { frontmatter: { draft: { ne: true } } }
     ) {
-      group(skip: $skip, limit: $limit, field: frontmatter___tags) {
+      group(field: frontmatter___tags) {
         fieldValue
         totalCount
       }
