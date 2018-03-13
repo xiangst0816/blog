2e82699c9c3aedd4b82a9bbced7d79336dec7c5b
向松涛 <280304286@163.com>
2018-03-13 13:20:37 +0800

    chore: Footer添加

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,16 @@ export default class Footer extends React.Component {
                 <div className="inner">
                     <section className="credits">
                         <span className="credits-theme">
-                            Theme <a href="https://github.com/zutrinken/attila">Attila</a> by <a href="http://zutrinken.com" rel="nofollow">zutrinken</a>
+                            Theme <a href="https://github.com/zutrinken/attila">Attila</a> by <a
+                            href="http://zutrinken.com" rel="nofollow">zutrinken</a>
                         </span>
-                        <span className="credits-software">Published with <a href="https://www.gatsbyjs.org/">Gatsby</a></span>
+                        <span className="credits-software">Published with <a
+                            href="https://github.com/">Github</a></span>
                     </section>
                 </div>
+                <section className="footer-remark">Made with <span className="love">♥</span> by <a
+                    href="https://www.gatsbyjs.org/">Gatsby</a>. Since 2015-2017
+                </section>
             </footer>
         );
     }
